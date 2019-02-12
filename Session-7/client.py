e934879cf4e438dae6b1cc7f0aeb6e5660a74c16
paulapenedo <paula.penedo14@gmail.com>
2019-02-12 16:51:55 +0100

    new file

---
+++ Session-7/client.py
@@ -0,0 +1,23 @@
+#Programming our first client
+import socket
+
+#Create a socket for communicating woth the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("Socket created")
+
+PORT = 8080
+IP = "212.128.253.64"
+
+#Connect to the server
+s.connect((IP, PORT))
+
+s.send(str.encode("HELLO FROM MY CLIENT")) #convert a string in a
+
+msg = s.recv(2048).decode("utf-8")
+print("MESSAGE FROM THE SERVER")
+print(msg)
+
+s.close()
+
+print("The end")
