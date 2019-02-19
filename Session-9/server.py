8afbbb8718ed7a6013b3911d9d0496d8ff9e9cc5
Paula <paula.penedo14@gmail.com>
2019-02-19 15:36:05 +0100

    new file

--- Session-9/server.py
+++ Session-9/server.py
@@ -4,4 +4,25 @@ PORT = 8080
 IP = "212.128.253.64"
 MAX_OPEN_REQUEST = 5
 
+#Create a socket for connectign to the client
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.socket.bind((IP, PORT))
+
+serversocket.listen(MAX_OPEN_REQUEST)
+
+print("Socket ready: {}".format(serversocket))
+
+
+
+
+while True:
+    print("Waiting for connection at: {}, {}".format(IP, PORT))
+    (clientsocket, address) = serversocket.accept()
+
+    # ...Process the client request
+    # Connection received. A new socket is returned for communicating with the client
+    print("Attending connections from client: {}".format(address))
+
+    clientsocket.close()
+    
