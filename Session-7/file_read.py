f9520484ec98a4d4f4b6ef0ade4dc7f6170ded08
Paula <paula.penedo14@gmail.com>
2019-02-26 12:50:38 +0100

    session 7

--- Session-7/file_read.py
+++ Session-7/file_read.py
@@ -1,13 +1,21 @@
-#Example of reading a file located in our local filesystem
+# Example of reading a file located in our local filesystem
 
 NAME = "mynotes.txt"
 
-#open the file
+# open the file
 myfile = open(NAME, 'r')
 
+# --my file is an object, let's see what is inside
+# print the filename
 print("File opened:  {}".format(myfile.name))
 
+# read the whole file into a string
 contents = myfile.read()
 
 print("The file contents are: {}".format(contents))
+print("The end!")
 
+# close the file
+myfile.close()
+
+# my file is an object, and it contains inside many methods and properties
