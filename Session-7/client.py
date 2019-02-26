00bb709e3a3b39b3f51ee1980a2ea42fe96bf0cb
Paula <paula.penedo14@gmail.com>
2019-02-26 12:50:02 +0100

    session 7

--- Session-7/client.py
+++ Session-7/client.py
@@ -1,21 +1,31 @@
-#Programming our first client
+# Programming our first client
 import socket
 
-#Create a socket for communicating woth the server
+# Create the socket for communicating with the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("Socket created")
 
+# print some information about the socket
+print()
+print("Socket created:")
+print(s)
+print("End")
+
+# we are setting our first connection, so we need to specify the IP address and the port
 PORT = 8080
 IP = "212.128.253.64"
 
 #Connect to the server
 s.connect((IP, PORT))
 
-s.send(str.encode("HELLO FROM MY CLIENT")) #convert a string in a
+# send data, no strings.
+# it is necesary to encode the string into bytes
+s.send(str.encode("HELLO FROM THE CLIENT"))
 
+
+# receive data from the server
 msg = s.recv(2048).decode("utf-8")
-print("MESSAGE FROM THE SERVER")
+print("MESSAGE FROM THE SERVER:\n")
 print(msg)
 
 s.close()
