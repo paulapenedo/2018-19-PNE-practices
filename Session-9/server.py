e0706abb439bdf3a6ccc27f112281517d21a26e8
Paula <paula.penedo14@gmail.com>
2019-03-09 20:15:33 +0100

    session 9

--- Session-9/server.py
+++ Session-9/server.py
@@ -1,28 +1,31 @@
 import socket
 
-PORT = 8080
-IP = "212.128.253.64"
+# configure the server's IP and PORT
+PORT = 8085
+IP = "192.168.1.36"
 MAX_OPEN_REQUEST = 5
 
-#Create a socket for connectign to the client
+# Create a socket for connecting to the client
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.socket.bind((IP, PORT))
+# bind the socket to the IP and PORT
+serversocket.bind((IP, PORT))
 
+# Configure the server sockets
+# MAX_OPEN:REQUESTS connect requests before refusing outside connections
 serversocket.listen(MAX_OPEN_REQUEST)
 
 print("Socket ready: {}".format(serversocket))
 
 
-
-
 while True:
+    # accept connections from outside
+    # the server is waiting for connections
     print("Waiting for connection at: {}, {}".format(IP, PORT))
     (clientsocket, address) = serversocket.accept()
 
-    # ...Process the client request
     # Connection received. A new socket is returned for communicating with the client
     print("Attending connections from client: {}".format(address))
 
+    # this server do nothing. The new socket is closed
     clientsocket.close()
-    
