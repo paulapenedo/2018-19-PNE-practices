ec1fe97172bb9d955e1b8a1f3bb9c601e7609263
paulapenedo <paula.penedo14@gmail.com>
2019-03-26 16:25:57 +0100

    not finished

---
+++ Session-19/exercise2.py
@@ -0,0 +1,60 @@
+import http.client
+import json
+
+# -- API information
+HOSTNAME = "www.metaweather.com"
+woeid = input("Introduce a city: ")
+ENDPOINT = "/api/location/search/?query={}".format(woeid)
+
+
+
+
+METHOD = "GET"
+
+# -- Here we can define special headers if needed
+headers = {'User-Agent': 'http-client'}
+
+# -- Connect to the server
+# -- NOTICE it is an HTTPS connection!
+# -- If we do not specify the port, the standar one
+# -- will be used
+conn = http.client.HTTPSConnection(HOSTNAME)
+
+# -- Send the request. No body (None)
+# -- Use the defined headers
+conn.request(METHOD, woeid + ENDPOINT, None, headers)
+
+# -- Wait for the server's response
+r1 = conn.getresponse()
+
+# -- Print the status
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+
+# -- Read the response's body and close
+# -- the connection
+text_json = r1.read().decode("utf-8")
+conn.close()
+
+# -- Optionally you can print the
+# -- received json file for testing
+# print(text_json)
+
+# -- Generate the object from the json file
+weather = json.loads(text_json)
+
+# -- Get the data
+time = weather['time']
+
+
+temp0 = weather['consolidated_weather'][0]
+description = temp0['weather_state_name']
+temp = temp0['the_temp']
+place = weather['title']
+
+print()
+print("Place: {}".format(place))
+print("Time: {}".format(time))
+print("Weather description: {}".format(description))
+print("Current temp: {} degrees".format(temp))
