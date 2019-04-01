6513588ca894dadc43abb06923af95bc92d14ec0
Paula <paula.penedo14@gmail.com>
2019-04-01 19:31:36 +0200

    session 19

--- Session-19/exercise2.py
+++ Session-19/exercise2.py
@@ -1,60 +1,54 @@
 import http.client
 import json
+import sys
 
 # -- API information
+
+city = input("Introduce a capital city: ")
 HOSTNAME = "www.metaweather.com"
-woeid = input("Introduce a city: ")
-ENDPOINT = "/api/location/search/?query={}".format(woeid)
-
-
-
-
+ENDPOINT = "/api/location/search/?query=" + city
 METHOD = "GET"
 
 # -- Here we can define special headers if needed
 headers = {'User-Agent': 'http-client'}
 
-# -- Connect to the server
-# -- NOTICE it is an HTTPS connection!
-# -- If we do not specify the port, the standar one
-# -- will be used
 conn = http.client.HTTPSConnection(HOSTNAME)
-
-# -- Send the request. No body (None)
-# -- Use the defined headers
-conn.request(METHOD, woeid + ENDPOINT, None, headers)
+conn.request(METHOD, ENDPOINT, None, headers)
 
 # -- Wait for the server's response
 r1 = conn.getresponse()
 
-# -- Print the status
-print()
-print("Response received: ", end='')
-print(r1.status, r1.reason)
-
 # -- Read the response's body and close
 # -- the connection
+capital = r1.read().decode("utf-8")
+conn.close()
+info = json.loads(capital)
+
+if len(info) == 0:
+    print("The city does not exist or the web does not have the information")
+    sys.exit()
+
+woeid = info[0]['woeid']
+
+
+ENDPOINT = "/api/location/"
+conn.request(METHOD, ENDPOINT + str(woeid) + '/', None, headers)
+r1 = conn.getresponse()
+
 text_json = r1.read().decode("utf-8")
 conn.close()
 
-# -- Optionally you can print the
-# -- received json file for testing
-# print(text_json)
-
-# -- Generate the object from the json file
 weather = json.loads(text_json)
-
-# -- Get the data
 time = weather['time']
-
-
 temp0 = weather['consolidated_weather'][0]
 description = temp0['weather_state_name']
 temp = temp0['the_temp']
 place = weather['title']
+sun_set = weather['sun_set']
 
 print()
 print("Place: {}".format(place))
 print("Time: {}".format(time))
+print("Sunset time: {}".format(sun_set))
 print("Weather description: {}".format(description))
 print("Current temp: {} degrees".format(temp))
